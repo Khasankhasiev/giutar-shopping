66ef7a74e71501054119667d122f687accb15d78
Khasan <mr.khasiev@list.ru>
2023-11-23 18:20:50 +0300

    create products, header, local storage files and added functionality

---
+++ components/Shopping/Shopping.js
@@ -0,0 +1,29 @@
+class Shopping {
+  constructor() {}
+
+  render() {
+    const productsStore = localStorageUtil.getProducts();
+    let htmlCatalog = "";
+
+    CATALOG.forEach(({ id, name, price }) => {
+      if (productsStore.indexOf(id) !== -1) {
+        htmlCatalog += `
+        <tr>
+            <td class = 'shopping-element__name'>⚡️ ${name}</td>
+            <td class = 'shopping-element__price'>${price.toLocaleString()} USD</td>
+        </tr>`;
+      }
+    });
+
+    const html = `
+    <div class = 'shopping-container'>
+      <table> 
+         ${htmlCatalog}
+      </table>
+    </div>
+    `;
+    ROOT_SHOPPING.innerHTML = html;
+  }
+}
+
+const shoppingPage = new Shopping();
